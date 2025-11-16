9c2d8667ed5710faf8353d01f4968544ffde48b8
Alex Verrico <contact@alexverrico.com>
2025-11-16 17:06:39 +1100

    Graph improvements

--- main.py
+++ main.py
@@ -91,6 +91,9 @@ async def graph(interaction: discord.Interaction, component: str, hours: int):
         plt.plot([row[3] for row in output], [row[2] for row in output])
         last_annotated = None
         count_since_annotated = 0
+        annotate_pos = 'top'
+        colors = ('red', 'blue', 'green')
+        annotate_color = 0
         for row in output:
             count_since_annotated += 1
             if last_annotated == row[2] and count_since_annotated < 5:
@@ -98,8 +101,16 @@ async def graph(interaction: discord.Interaction, component: str, hours: int):
             if last_annotated is not None and count_since_annotated < 5:
                 if last_annotated * 1.01 > row[2] > last_annotated * 0.999:
                     continue
-            plt.annotate(xy=(transform_date(row[0]), row[2]), text=row[2])
+            plt.annotate(xy=(transform_date(row[0]), row[2]), text=row[2], textcoords='offset fontsize',
+                         color=colors[annotate_color],
+                         xytext=(0, 0.2 if annotate_pos == 'top' else -1.2),
+                         arrowprops={'width': 0.1, 'headwidth': 4, 'headlength': 3, 'shrink': .05,
+                                     'color': colors[annotate_color]})
             last_annotated = row[2]
+            if annotate_pos == 'top': annotate_pos = 'bottom'
+            elif annotate_pos == 'bottom': annotate_pos = 'top'
+            annotate_color += 1
+            if annotate_color == 3: annotate_color = 0
             count_since_annotated = 0
         plt.ylim(monitored_fields[component]['limits']['min'], monitored_fields[component]['limits']['max'])
         plt.xlabel('Time', fontsize=16)
